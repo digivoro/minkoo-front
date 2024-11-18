c5d3fbff015bba8a1449c53ab12e72ad23f2e2b3
Felipe Richter <frichterc@gmail.com>
2024-11-18 10:54:28 -0300

    feat: update community store

--- store/communities.ts
+++ store/communities.ts
@@ -1,16 +1,15 @@
 import type { Community } from "~/types";
 
-export const useCommunitiesStore = defineStore("communities", () => {
+export const useCommunityStore = defineStore("community", () => {
+  const API_URL = "http://localhost:3001/communities";
   const communities = ref<Community[]>([]);
+  const community = ref<Community | null>(null);
 
   async function getCommunities() {
     try {
-      const response: Community[] = await $fetch(
-        "http://localhost:3001/communities",
-        {
-          method: "GET",
-        },
-      );
+      const response: Community[] = await $fetch(API_URL, {
+        method: "GET",
+      });
       communities.value = response;
     } catch (error) {
       communities.value = [];
@@ -18,5 +17,17 @@ export const useCommunitiesStore = defineStore("communities", () => {
     }
   }
 
-  return { communities, getCommunities };
+  async function getCommunity(id: number) {
+    try {
+      const response: Community = await $fetch(API_URL + "/" + id, {
+        method: "GET",
+      });
+      community.value = response;
+    } catch (error) {
+      community.value = null;
+      throw error;
+    }
+  }
+
+  return { communities, community, getCommunities, getCommunity };
 });
