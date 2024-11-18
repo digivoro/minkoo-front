61af87e550dbdcb1faaf32242cc09e55105f0a64
Felipe Richter <frichterc@gmail.com>
2024-11-18 10:53:30 -0300

    feat: add user type

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,3 +5,18 @@ export interface Community {
   createdAt?: string;
   updatedAt?: string;
 }
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  identificationNumber: string;
+  phone: string;
+  profilePic: string;
+}
+
+export interface LoginResponse {
+  access_token: string | null;
+  user: User | null;
+}
