f70ef0e3816892eb21a24d0df98e0454954ea5c3
Felipe Richter <frichterc@gmail.com>
2024-11-19 17:29:50 -0300

    feat: add community page layout

---
+++ layouts/community.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <AppHeader>
+      <div class="min-h-screen pb-12">
+        <slot />
+      </div>
+    </AppHeader>
+    <AppFooter />
+  </div>
+</template>
--- pages/comunidad/[id].vue
+++ pages/comunidad/[id].vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { useCommunityStore } from "~/store/communities";
 
+definePageMeta({
+  layout: "community",
+});
+
 const route = useRoute();
 const communityStore = useCommunityStore();
 const { community } = storeToRefs(communityStore);
@@ -36,7 +40,49 @@ onMounted(async () => {
 
   <!-- Display -->
   <template v-else-if="community">
-    <PageTitle class="mb-8">{{ community.name }}</PageTitle>
-    <CommunityCard :community="community" />
+    <!-- Hero -->
+    <div class="hero relative mb-8 h-60">
+      <figure
+        class="hero-overlay bg-gradient-to-r from-black via-transparent opacity-70"
+      ></figure>
+
+      <div class="absolute left-0 top-0 w-full pl-4 pt-4">
+        <div class="text-xs font-bold text-neutral-content">COMUNIDAD</div>
+        <p class="mb-8 text-4xl text-neutral-content">
+          {{ community.name }}
+        </p>
+      </div>
+    </div>
+
+    <!-- Content -->
+    <div class="grid grid-cols-3 px-4">
+      <div class="card bg-neutral shadow-lg">
+        <div class="card-body text-neutral-content">
+          <p class="card-title text-sm">Información</p>
+          <p>
+            {{ community.description }}
+          </p>
+          <div class="divider divider-secondary"></div>
+          <div class="card-actions">
+            <div class="flex w-full justify-between">
+              <div class="flex items-center">
+                <IconLockOpen class="mr-2 size-4" />
+                <p class="font-semibold">Comunidad abierta</p>
+              </div>
+              <button class="btn btn-primary btn-sm">
+                <IconUserGroup class="size-4" /> Unirte
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </template>
 </template>
+
+<style scoped>
+.hero {
+  background-image: url("https://drgoodprice.com/wp-content/uploads/2024/04/dr-good-comunidad-unida-united-community-1.webp");
+  background-size: cover;
+}
+</style>
