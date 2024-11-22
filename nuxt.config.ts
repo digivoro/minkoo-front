64f691ccde29f0dbced29b732bb0c136b6efe249
Felipe Richter <frichterc@gmail.com>
2024-11-21 22:18:54 -0300

    feat: add meta for page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Minkoo",
+    },
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
