aa1535209806ab4a16434f4b77d1451a6d21ff68
Felipe Richter <frichterc@gmail.com>
2024-12-13 23:37:58 -0300

    feat: add basic mapbox

---
+++ components/App/Map.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import mapboxgl from "mapbox-gl";
+
+const config = useRuntimeConfig();
+
+mapboxgl.accessToken = config.public.mapboxToken;
+
+let map;
+
+const mapContainer = useTemplateRef<HTMLDivElement>("map-container");
+onMounted(() => {
+  if (mapContainer.value) {
+    console.log("C");
+    const mapbox = new mapboxgl.Map({
+      container: mapContainer.value,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [-71.224518, 42.213995],
+      zoom: 9,
+    });
+
+    map = mapbox;
+  }
+});
+
+onUnmounted(() => {});
+</script>
+
+<template>
+  <div class="flex" ref="map-container"></div>
+</template>
--- pages/comunidades/crear.vue
+++ pages/comunidades/crear.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1>Crear comunidad</h1>
+    <AppMap />
   </div>
 </template>
